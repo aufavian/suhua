0f1077e39b84272f4b3f2084d41f9b41a879e71d
aufavian <aufavian.2004@gmail.com>
2024-09-25 10:13:50 +0700

    Update suuhuuu.py

--- suuhuuu.py
+++ suuhuuu.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-x = st.number_input ("Masukkan suhu")
-sx = st.text_input("Satuan", "C")
-st.write("Anda menginput suhu ", x, "dengan satuan ", sx)
-sy = st.text_input("Konversi ke", "C")
-if (sx == "C"):
+x = st.number_input("Masukkan suhu")
+sx = st.text_input("Satuan", "c")
+st.write("Anda menginput suhu ", x," dengan satuan ", sx )
+sy = st.text_input("Konversi ke", "c")
+if (sx == "c"):
   pass
 else:
   pass
 
-st.write("Hasil konversi dari ",x,sx, " adalah...", sy)
+st.write("Hasil konversi dari ",x,sx," adalah ...", sy )
